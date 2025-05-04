07103ab5a194687845de1fd0b7c6eeb696544f36
Николай С <nicksrnk@gmail.com>
2025-05-04 23:07:40 +0500

    - cicd

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,11 +1,9 @@
-// export let host: string = `${document.location.protocol.slice(0, -1)}://${document.location.host.split(':')[0]}`
-//
-// if (document.location.host.split(':')[1]) {
-//     if (document.location.host.split(':')[1] !== '3000')
-//         host += ':' + document.location.host.split(':')[1]
-//     else
-//         host += ':8000'
-// } else
-//     host += ''
+export let host: string = `${document.location.protocol.slice(0, -1)}://${document.location.host.split(':')[0]}`
 
-export let host: string = 'project-de401t-ar-education-api-93a4.twc1.net';
+if (document.location.host.split(':')[1]) {
+    if (document.location.host.split(':')[1] !== '3000')
+        host += ':' + document.location.host.split(':')[1]
+    else
+        host += ':8000'
+} else
+    host += ''
