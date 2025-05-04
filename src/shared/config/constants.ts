5e0a51291af7d18f965a421a445db909dd124489
Николай С <nicksrnk@gmail.com>
2025-05-05 00:09:09 +0500

    - cicd

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,9 +1,11 @@
-export let host: string = `${document.location.protocol.slice(0, -1)}://${document.location.host.split(':')[0]}`
+// export let host: string = `${document.location.protocol.slice(0, -1)}://${document.location.host.split(':')[0]}`
+//
+// if (document.location.host.split(':')[1]) {
+//     if (document.location.host.split(':')[1] !== '3000')
+//         host += ':' + document.location.host.split(':')[1]
+//     else
+//         host += ':8000'
+// } else
+//     host += ''
 
-if (document.location.host.split(':')[1]) {
-    if (document.location.host.split(':')[1] !== '3000')
-        host += ':' + document.location.host.split(':')[1]
-    else
-        host += ':8000'
-} else
-    host += ''
+export let host:string = 'http://83.222.9.213:7788'
