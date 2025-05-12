0162ae57a813afeccc0e893d1c5205734cd3f18b
Николай С <nicksrnk@gmail.com>
2025-05-12 10:17:29 +0500

    - Изменены порты

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -2,7 +2,7 @@ export let host: string = `${document.location.protocol.slice(0, -1)}://${docume
 
 if (document.location.host.split(':')[1]) {
     if (document.location.host.split(':')[1] !== '3000')
-        host += ':6868'
+        host += ':7788'
     else
         host += ':8000'
 } else
